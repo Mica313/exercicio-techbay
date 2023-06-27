5342fa8ffc11671ca25ff897afe460faf1991844
Micael Almeida <onixg8874@gmail.com>
2023-06-27 19:45:31 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    
-})
+    $('.menuH').click(function(){
+        $('nav').slideToggle()
+    });
+    $('.lista button').click(function(){
+        alert('!! item adicionado ao carrinho !!')
+    });
+});
